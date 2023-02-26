dee059b4445d4c62c6689c408cca6b1ff4d2ea46
Flash3r01 <44031456+Flash3r01@users.noreply.github.com>
2023-02-26 14:56:34 +0100

    optimize images for index page

--- src/scripts/build/imageImports.ts
+++ src/scripts/build/imageImports.ts
@@ -8,3 +8,10 @@ export { default as littleLightEngine_noHitbox } from "@images/games/littleLight
 export { default as littleLightEngine_hitbox } from "@images/games/littleLightEngine/hitbox.png"
 export { default as littleLightEngine_depthmap } from "@images/games/littleLightEngine/depthmap.png"
 export { default as littleLightEngine_background } from "@images/games/littleLightEngine/background.png"
+
+// Icons
+export { default as starFull } from "@images/star_full.png"
+export { default as starEmpty } from "@images/star_empty.png"
+
+// Miscellaneous
+export { default as pictureOfMe } from "@images/pictureOfMe.jpeg"
