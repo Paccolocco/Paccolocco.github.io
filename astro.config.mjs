252af7d2061dd8f7d26a3f82b1b5ec3ae8ae6c60
Lenny Siol <44031415+Paccolocco@users.noreply.github.com>
2023-05-26 22:15:02 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lennysiol.com/',
+  base: 'https://github.com/Paccolocco/Paccolocco.github.io',
   integrations: [image({
     serviceEntryPoint: "@astrojs/image/sharp"
   })]
